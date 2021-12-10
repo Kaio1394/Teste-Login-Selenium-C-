5f75f908d6e1d91c63b143e662b549fd96c751f2
kaio <kaio.santiago.13@hotmail.com>
2021-12-10 13:11:07 -0300

    Refatorando script

---
+++ TestProject2/Helpers/TestHelpers.cs
@@ -0,0 +1,24 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Reflection;
+using System.Text;
+
+namespace TestProject2.Helpers
+{
+    public class TestHelpers
+    {
+        public static string PastaDoExecutavel()
+        {
+            var driver = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
+            return driver;
+        }
+        public static void AcessarPaginaFaceook()
+        {
+            IWebDriver driver = new ChromeDriver(PastaDoExecutavel());
+            driver.Navigate().GoToUrl("https://www.facebook.com");
+        }
+    }
+}
--- TestProject2/TestLoginFacebook.cs
+++ TestProject2/TestLoginFacebook.cs
@@ -3,31 +3,37 @@ using OpenQA.Selenium.Chrome;
 using System;
 using System.IO;
 using System.Reflection;
+using TestProject2.Helpers;
 using Xunit;
 
 namespace TestProject2
 {
     public class TestLoginFacebook
     {
+        private ChromeDriver driver;
+
+        public TestLoginFacebook()
+        {
+            driver = new ChromeDriver(TestHelpers.PastaDoExecutavel());
+        }
         [Fact]
         public void DadoChromeAbertoDeveAcessarPaginaFacebookERealizarLogin()
         {
             //Dado que eu acesso a página do facebook
-            IWebDriver driver = 
-                new ChromeDriver(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location));
             driver.Navigate().GoToUrl("https://www.facebook.com");
 
-            //Quando eu preecho dos campso de login.
+            //Quando eu preecho os campos de login.
+           
+
             driver.FindElement(By.Id("email")).SendKeys("xxxxxxxx@hotmail.com");
             driver.FindElement(By.Id("pass")).SendKeys("xxxxxxxxxxxx");
-            
-            //E clico no botão Entrar.
-            driver.FindElement(By.Name("login")).Click();
 
-            //Então Capturo o título da página
+            //E clico no botão Entrar e capturo o título da página.
+            driver.FindElement(By.Name("login")).Click();
             var tituloEsperado = driver.Title;
             var tituloCapturado = "Facebook";
 
+            //Então Capturo o título da página
             Assert.Contains(tituloCapturado, driver.Title);
         }
     }
